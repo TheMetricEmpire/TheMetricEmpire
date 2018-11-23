310750831937010339022db4c6f838a4b9216b10
NyksterR <Nykster@gmail.com>
2018-11-23 16:55:10 -0500

    Update gatsby-config.js
    Adding public to pathprefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     author: "Hunter Chang",
     description: "A Gatsby.js Starter based on Photon by HTML5 UP"
   },
-  pathPrefix: 'themetricempire.github.io/themetricempire',
+  pathPrefix: 'themetricempire.github.io/themetricempire/public',
   plugins: [
     'gatsby-plugin-react-helmet',
     {
