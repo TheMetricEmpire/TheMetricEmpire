ce8f18c822e4076a540a631bc6a6a6470d103fa1
plpapineau <40634014+plpapineau@users.noreply.github.com>
2018-11-29 16:31:37 -0500

    Actually adding ReactGA support

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import icon from '../assets/images/header_icon.png'
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-130088165-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 class Header extends React.Component {
     render() {
         return (
