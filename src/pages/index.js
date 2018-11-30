7e0bc1318d46b4bfd2d28a44e6300edfec22dfa3
plpapineau <40634014+plpapineau@users.noreply.github.com>
2018-11-30 10:17:39 -0500

    Fixing circular redirects in cannonical URLs.

--- src/pages/index.js
+++ src/pages/index.js
@@ -106,13 +106,13 @@ class Homepage extends React.Component {
                 <meta property="og:title"       content="Metric Empire Game Studio"/>
                 <meta property="og:image"       content="https://www.metricempire.com/static/OG_Image-f95221e982d90376da9ebe21dd5ca096.jpg"/>
                 <meta property="og:description" content="Metric Empire is a game studio that crafts infinitely fun, savantly-distilled, innovative games. Join our newsletter for details of our upcoming game!"/>
-                <meta property="og:url"         content="http://www.metricempire.com"/>
+                <meta property="og:url"         content="http://www.metricempire.com/"/>
                 <meta property="og:type"        content="website"/>
                 <meta property="fb:app_id"      content=""/>
                 <meta name="twitter:card"       content="summary"/>
                 <meta name="twitter:title"      content="Metric Empire Game Studio"/>
                 <meta name="twitter:description"content="Metric Empire is a game studio that crafts infinitely fun, savantly-distilled, innovative games. Join our newsletter for details of our upcoming game!"/>
-                <meta name="twitter:url"        content="http://www.metricempire.com"/>
+                <meta name="twitter:url"        content="http://www.metricempire.com/"/>
                 <meta name="twitter:image"      content="https://www.metricempire.com/static/OG_Image-f95221e982d90376da9ebe21dd5ca096.jpg"/>
                 </Helmet>
 
