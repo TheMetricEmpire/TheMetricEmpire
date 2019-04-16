f3f247777ac272028b7857f800394039849493af
plpapineau <40634014+plpapineau@users.noreply.github.com>
2019-04-15 21:01:01 -0400

    Update Header.js
    Added linkback to mainpage on header logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
         return (
             <section id="header">
                 <div className="inner">
-                    <span className="image"><img src={icon} style={{width: 250, height: 250}} alt="Metric Empire Company Logo" /></span>
+                    <span className="image"><a href="../"><img src={icon} style={{width: 250, height: 250}} alt="Metric Empire Company Logo" /></a></span>
                     <h1>Metric Empire</h1>
                     <h3>Game Studio</h3>
                 </div>
