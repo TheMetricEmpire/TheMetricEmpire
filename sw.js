03553d1d0918eb9f05574fce7cc9d3b66dbc9071
plpapineau <plpapineau@metricempire.com>
2021-05-02 21:12:00 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-434ec6d04ab95831d3ad.js"
+    "url": "webpack-runtime-efbe0556120fd5fb2af5.js"
   },
   {
     "url": "app-7feb649893d2338156e5.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6ace207e3b1196ba288f1b84ea2c7eac"
+    "revision": "d0d41e698a9a38fbcb264db731e24185"
   },
   {
     "url": "component---src-pages-404-js-676b6cbd7ee62982dc26.js"
